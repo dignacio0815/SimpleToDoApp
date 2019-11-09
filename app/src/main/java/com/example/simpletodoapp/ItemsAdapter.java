443a6908f3fabc5ee1eed89d6205295ba8ca0e24
Denize Ignacio <denizeignacio@gmail.com>
2019-11-09 11:50:26 -0800

    Allow user to click past entries and update them

--- app/src/main/java/com/example/simpletodoapp/ItemsAdapter.java
+++ app/src/main/java/com/example/simpletodoapp/ItemsAdapter.java
@@ -11,16 +11,21 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
+    public interface OnClickListener {
+        void onItemClicked(int position);
+    }
     public interface OnLongClickListener {
         void onItemLongClicked(int position);
     }
 
     List<String> items;
     OnLongClickListener longClickListener;
+    OnClickListener clickListener;
 
-    public ItemsAdapter(List<String> items, OnLongClickListener longClickListener) {
+    public ItemsAdapter(List<String> items, OnLongClickListener longClickListener, OnClickListener clickListener ) {
         this.items = items;
         this.longClickListener = longClickListener;
+        this.clickListener = clickListener;
     }
 
     @NonNull
@@ -56,7 +61,13 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> 
         }
         public void bind(String item) {
             tvItem.setText(item);
-            tvItem.setOnLongClickListener( new View.OnLongClickListener() {
+            tvItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    clickListener.onItemClicked(getAdapterPosition());
+                }
+            });
+            tvItem.setOnLongClickListener(new View.OnLongClickListener() {
                 @Override
                 public boolean onLongClick(View v) {
                     // remove the item from the recycler view
